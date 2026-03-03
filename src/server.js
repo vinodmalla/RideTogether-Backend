99b6b3c31b7a402a07fae05cbe849fd6e447ce9f
vinod Malla <mallavinod95@gmail.com>
2026-03-03 18:57:44 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -16,12 +16,12 @@ app.use(cors(
 app.use(express.json()); //enabling JSON parsing for incoming requests
 app.use('/',createRoomRoute); //using the createRoomRoute for handling routes related to creating a room
  //connecting to the MongoDB database by calling the connectDB function
- 
+const PORT = process.env.PORT || 5000;
 initlizeSocket(server); //initializing the socket connection by passing the server as an argument to the initlizeSocket function
 connectDB().then(()=>{
     console.log("Database connection established"); 
-    server.listen(5000,()=>{
-    console.log('Server is running on port 5000'); //starting the server and listening on port 5000
+    server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`); //starting the server and listening on port 5000
 });
 }).catch((error)=>{
     console.error("Database connection failed:",error);
